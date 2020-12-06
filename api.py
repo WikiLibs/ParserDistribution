30a1dbaf59e15d2caaffc5ce2ad29453a2742913
William Tessari <william.tessari@epitech.eu>
2020-12-06 13:37:27 +0100

    api init

--- api.py
+++ api.py
@@ -1,11 +1,22 @@
 import flask
+from flask import request
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 
 @app.route('/', methods=['GET'])
+
 def home():
-    return "<h1>Petite initialisation</h1><p>Voilà Nicolas amuse toi avec ça! </p>"
+    os = request.args.get("os")
+    apiKey = request.args.get("apiKey")
+    userID = request.args.get("userId")
+
+    if os and apiKey and userID:
+        print("[SUCCESS] OS: " + str(os) + ", apiKey: " + str(apiKey) + ", userID: " + str(userID))
+    else:
+        print("[WARNING] ARGS MISSING")
+
+    return "<h1>Petite initialisation</h1><p>Voilà Nicolas amuse toi avec ça! Ca m'a effectivement pris 5 min X) </p>"
 
 app.run()
