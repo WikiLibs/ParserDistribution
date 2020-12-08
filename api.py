6dc3439b67c676ba112dff9ecab6df81c08d7b21
William Tessari <william.tessari@epitech.eu>
2020-12-08 19:05:42 +0100

    dowloading success

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 import flask
-from flask import request
+import os
+from flask import request, send_file
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -8,15 +9,28 @@ app.config["DEBUG"] = True
 @app.route('/', methods=['GET'])
 
 def home():
-    os = request.args.get("os")
+    pathToBin = "./patch-tool"
+    osName = request.args.get("os")
     apiKey = request.args.get("apiKey")
     userID = request.args.get("userId")
 
-    if os and apiKey and userID:
-        print("[SUCCESS] OS: " + str(os) + ", apiKey: " + str(apiKey) + ", userID: " + str(userID))
-    else:
-        print("[WARNING] ARGS MISSING")
+    if osName and apiKey and userID:
+        os.system(pathToBin + ' ' + str(osName) + ' ' + str(apiKey) + ' ' + str(userID) + ' > tmp')
 
-    return "<h1>Petite initialisation</h1><p>Voilà Nicolas amuse toi avec ça! Ca m'a effectivement pris 5 min X) </p>"
+        f = open("tmp", "r")
+        content = f.read()
+        f.close()
+        os.system('rm tmp')
+
+        if len(content) == 0:
+            return {'error': "invalid args"}
+        
+        try:
+            return send_file(content.replace('\n', ''), attachment_filename='wikilibs_parser', as_attachment=True)
+        except Exception as e:
+            return {'error': str(e)}
+        
+    else:
+        return {'error': "missing args"}
 
 app.run()
